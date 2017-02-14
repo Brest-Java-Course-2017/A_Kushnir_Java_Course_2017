e3b6621afa343b0720743ed46fe6c704fa866578
ArtsiomKushnir <artsiomkush@gmail.com>
2017-02-14 10:54:29 +0300

    modified UserTest.java for model

--- model/src/test/java/com/epam/test/model/UserTest.java
+++ model/src/test/java/com/epam/test/model/UserTest.java
@@ -10,26 +10,36 @@ import static org.junit.Assert.*;
 public class UserTest {
 
     public static final int USER_ID = 11;
+    public static final String USER_LOGIN = "Login";
+    public static final String USER_PASSWORD = "Password";
+    public static final String USER_DESCRIPTION = "Description";
+    public User user = new User();
+
     @org.junit.Test
     public void getUserID() throws Exception {
-        User user = new User();
         user.setUserID(11);
+        // ожидаемое значение
         Assert.assertEquals("User id: ", (Integer) USER_ID, user.getUserID());
     }
 
     @org.junit.Test
     public void getLogin() throws Exception {
-
+        user.setLogin("Login");
+        // ожидаемое значение
+        Assert.assertEquals("User id: ", USER_LOGIN, user.getLogin());
     }
 
     @org.junit.Test
     public void getPassword() throws Exception {
-
+        user.setPassword("Password");
+        // ожидаемое значение
+        Assert.assertEquals("User password: ", USER_PASSWORD, user.getPassword());
     }
 
     @org.junit.Test
     public void getDescription() throws Exception {
-
+        user.setDescription("Description");
+        // ожидаемое значение
+        Assert.assertEquals("User Description: ", USER_DESCRIPTION, user.getDescription());
     }
-
 }
