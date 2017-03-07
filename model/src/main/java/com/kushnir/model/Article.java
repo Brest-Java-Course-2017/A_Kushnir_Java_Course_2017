7ce21e2ef5280df24cfdc12f414b2109af9f4339
ArtsiomKushnir <artsiomkush@gmail.com>
2017-03-07 18:38:30 +0300

    added model level, created 2 entities: Journalist and Articles

---
+++ model/src/main/java/com/kushnir/model/Article.java
@@ -0,0 +1,99 @@
+package com.kushnir.model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ * model of Article
+ */
+public class Article {
+
+    private Integer     id;
+    private String      name;
+    private LocalDate   dateCreate;
+    private Integer     popularity;
+    private Integer     journalistId;
+
+    public Article() {
+    }
+
+    public Article(String name, LocalDate dateCreate, Integer popularity, Integer journalistId) {
+        this.name = name;
+        this.dateCreate = dateCreate;
+        this.popularity = popularity;
+        this.journalistId = journalistId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDateCreate() {
+        return dateCreate;
+    }
+
+    public Integer getPopularity() {
+        return popularity;
+    }
+
+    public Integer getJournalistId() {
+        return journalistId;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDateCreate(LocalDate dateCreate) {
+        this.dateCreate = dateCreate;
+    }
+
+    /*
+    * This method set dateCreate from string in format: "YYYY-MM-DD"
+    */
+    public void setDateCreateFromString(String dateCreate) {
+        this.dateCreate = LocalDate.parse(dateCreate);
+    }
+
+    public void setPopularity(Integer popularity) {
+        this.popularity = popularity;
+    }
+
+    public void setJournalistId(Integer journalistId) {
+        this.journalistId = journalistId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article)) return false;
+        Article article = (Article) o;
+        return Objects.equals(getName(), article.getName()) &&
+                Objects.equals(getDateCreate(), article.getDateCreate()) &&
+                Objects.equals(getPopularity(), article.getPopularity()) &&
+                Objects.equals(getJournalistId(), article.getJournalistId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getDateCreate(), getPopularity(), getJournalistId());
+    }
+
+    @Override
+    public String toString() {
+        return "Article{" +
+                "name='" + name + '\'' +
+                ", dateCreate=" + dateCreate +
+                ", popularity=" + popularity +
+                ", journalistId=" + journalistId +
+                '}';
+    }
+}
---
+++ model/src/main/java/com/kushnir/model/Journalist.java
@@ -0,0 +1,89 @@
+package com.kushnir.model;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+/**
+ * model of Journalist
+ */
+public class Journalist {
+
+    private Integer     id;
+    private String      name;
+    private Integer     rate;
+    private LocalDate   birthDate;
+
+    public Journalist() {
+    }
+
+    public Journalist(String name, Integer rate, LocalDate birthDate) {
+        this.name = name;
+        this.rate = rate;
+        this.birthDate = birthDate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getRate() {
+        return rate;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRate(Integer rate) {
+        this.rate = rate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    /*
+    * This method set birthDate from string in format: "YYYY-MM-DD"
+    */
+    public void setBirthDateFromString(String birthDate) {
+        this.birthDate = LocalDate.parse(birthDate);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Journalist)) return false;
+        Journalist that = (Journalist) o;
+        return Objects.equals(getId(), that.getId()) &&
+                Objects.equals(getName(), that.getName()) &&
+                Objects.equals(getRate(), that.getRate()) &&
+                Objects.equals(getBirthDate(), that.getBirthDate());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getRate(), getBirthDate());
+    }
+
+    @Override
+    public String toString() {
+        return "Journalist{" +
+                "id = " + id +
+                ", name = '" + name + '\'' +
+                ", rate = " + rate +
+                ", birthDate = " + birthDate +
+                '}';
+    }
+}
