0088d2ae2f6729c59329b96c52efa0736fb8e958
ArtsiomKushnir <artsiomkush@gmail.com>
2017-03-11 21:51:54 +0300

    little fix in data-script.sql

--- db/src/main/resources/data-script.sql
+++ db/src/main/resources/data-script.sql
@@ -7,7 +7,7 @@ VALUES
 
 INSERT INTO articles (id_article, naim, create_date, popularity, id_journalist)
 VALUES
-  (1, 'Chechnya. BUSINESS ON "EIGHTH MARCH"', '2005-12-05', 100, 4),
+  (1, 'Chechnya. BUSINESS ON "EIGHTH MARCH', '2005-12-05', 100, 4),
   (2, 'THE RALLY IS AGAINST CORRUPTION', '2006-05-18', 100, 4),
   (3, 'Moose Jaw, U.S.A.? Never! Jamais!', '1990-06-25', 90, 2),
   (4, 'TV opportunity for service at Geneva', '1985-11-28', 80, 2);
